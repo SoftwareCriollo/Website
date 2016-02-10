1a4c957bc392c248a6c348f38f88e6901c246335
felix <felix@softwarecriollo.com>
2016-02-10 00:48:03 -0430

    Implement in the web SC new website slicings
    Some progress
    
    The structure of the application was modified.
    Sinatra framework for implementation is used.

---
+++ env.rb
@@ -0,0 +1,5 @@
+ENV['EMAIL_TO'] = 'felix@softwarecriollo.com'
+ENV['EMAIL_USER_NAME'] = 'notifications@softwarecriollo.com'
+ENV['EMAIL_PASSWORD'] = 'letmein123456'
+ENV['EMAIL_DOMAIN'] = 'localhost'
+ENV['SUBJECT_MAIL'] = 'New lead Software Criollo'
