9d5e0ca50ef8d6a2e2d023bcc1a3dd1c433c19bd
felix <felix@softwarecriollo.com>
2016-02-15 15:41:32 -0430

    Improved code, and scripts

---
+++ public/js/all-initialized.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  smoothScroll.init();
+  $('.parallax').parallax();
+  $('.slider').slider({full_width: true});
+  $(".button-collapse").sideNav();
+  $('select').material_select();
+});
